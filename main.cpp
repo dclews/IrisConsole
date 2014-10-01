6f6c8a290ab15e896d875e82207c1a40d5d5b047
Dan Clews <Clueless_@live.co.uk>
2014-10-01 11:22:22 +0100

    Rework.

--- main.cpp
+++ main.cpp
@@ -7,47 +7,45 @@
 
 void loadPlugins(QDir pluginsDir, iris::Bot& bot)
 {
-    qDebug() << "Searching for plugins in folder: " << pluginsDir.absolutePath();
+	qDebug() << "Searching for plugins in folder:" << pluginsDir.absolutePath();
 
-    foreach (QString fileName, pluginsDir.entryList(QDir::Files)) {
-        qDebug() << fileName;
+	foreach (QString fileName, pluginsDir.entryList(QDir::Files)) {
+		QPluginLoader loader(pluginsDir.absoluteFilePath(fileName));
+		QObject* plugin = loader.instance();
 
-        QPluginLoader loader(pluginsDir.absoluteFilePath(fileName));
-        QObject* plugin = loader.instance();
+		if (plugin)
+		{
+			qDebug() << "\nFound plugin:" << fileName;
 
-        if (plugin)
-        {
-            qDebug() << "Found plugin: " << fileName;
-
-            CommandPlugin* commandPlugin = qobject_cast<CommandPlugin*>(plugin);
-            if(commandPlugin)
-            {
-                bot.registerCommandPlugin(commandPlugin);
-            }
-        }
-        else
-        {
-            qDebug() << "Invalid plugin." << loader.errorString();
-        }
-    }
+			CommandPlugin* commandPlugin = qobject_cast<CommandPlugin*>(plugin);
+			if(commandPlugin)
+			{
+				bot.registerCommandPlugin(commandPlugin);
+			}
+		}
+		else
+		{
+			qDebug() << "Invalid plugin." << loader.errorString();
+		}
+	}
 }
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+	QCoreApplication a(argc, argv);
 
-    QString name("POSIX_ME_HARDER");
-    iris::Bot bot;
+	QString name("Iris");
+	iris::Bot bot;
 
-    loadPlugins(QDir("/usr/local/share/iris/plugins/"), bot);
+	loadPlugins(QDir("/usr/local/share/iris/plugins/"), bot);
 
-    bot.setUserName(name);
-    bot.setNickName(name);
-    bot.setRealName(name);
-    bot.setHost("irc.w3.org");
-    bot.setPort(6665);
-    bot.joinChannel("#ec");
-    bot.open();
+	bot.setUserName(name);
+	bot.setNickName(name);
+	bot.setRealName(name);
+	bot.setHost("irc.w3.org");
+	bot.setPort(6665);
+	bot.joinChannel("#iris");
+	bot.open();
 
-    return a.exec();
+	return a.exec();
 }
